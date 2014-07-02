e3109510ad2f4ddeb90be7e714d478b7f24eb280
ntyet <tienyettoan@gmail.com>
2014-07-02 17:22:11 -0500

    R code and Presentation update

--- Rcode_Proteomics.R
+++ Rcode_Proteomics.R
@@ -1,5 +1,4 @@
 library("lme4")
-
 library("plyr")
 library("ggplot2")
 library("MASS")
@@ -21,34 +20,34 @@ y <- c( "Image","X", paste("X.", 1:31, sep = "")) # Column name unsued
 # Column name used in analysis including the id of protein and 16 gels  
 # of 8 animals, each gel runs twice
 
-use.col <- setdiff(x,y)
-use.col
-dat.used <- as.matrix(dat[, use.col])
-dim(dat.used)
+use_col <- setdiff(x,y)
+use_col
+dat_used <- as.matrix(dat[, use_col])
+dim(dat_used)
 
 
-# The first row is group classification, the second row is the name of Std. Abund, therefore the data actually in use is the dat.used except the first 2 rows
-dat.final <- matrix(as.numeric(dat.used[-c(1:2),]), 
-                    nrow = nrow(dat.used)-2,
-                    ncol = ncol(dat.used),
+# The first row is group classification, the second row is the name of Std. Abund, therefore the data actually in use is the dat_used except the first 2 rows
+dat_final <- matrix(as.numeric(dat_used[-c(1:2),]), 
+                    nrow = nrow(dat_used)-2,
+                    ncol = ncol(dat_used),
                     byrow = F) 
-dim(dat.final)
+dim(dat_final)
 
-head(dat.final)
+head(dat_final)
 
 # Group of each sample
 
-group <- dat.used[1,-1] 
+group <- dat_used[1,-1] 
 group
 
-# Obtain data for each group: Depelted and Not.Depleted (Whole), the first column of dat.final contains name of protein
-depleted.dat <- dat.final[,-1][,group=="Depleted"]
-dim(depleted.dat)
-depleted.dat[1,]
+# Obtain data for each group: Depelted and Not.Depleted (Whole), the first column of dat_final contains name of protein
+deplete <- dat_final[,-1][,group=="Depleted"]
+dim(deplete)
 
-not.depleted.dat <- dat.final[,-1][,group=="Whole"]
-dim(not.depleted.dat)
-not.depleted.dat[1,]
+
+whole <- dat_final[,-1][,group=="Whole"]
+dim(whole)
+
 
 # Find out which Cy is used for each run
 
@@ -64,7 +63,7 @@ group[group=="Whole"] # Cy for Whole group: rep(c(3,3,5,5),4)
 # x <- rnorm(16, 0, 1)
 # depleted <- "TRUE"
 
-result <- function(x, depleted){ # x is the row of data (i.e., data of each protein spot)
+out_model <- function(x, depleted){ # x is the row of data (i.e., data of each protein spot)
   if (depleted == "TRUE"){
     cy <- as.factor(rep(c(5,5,3,3),4))
   } else {
@@ -73,146 +72,65 @@ result <- function(x, depleted){ # x is the row of data (i.e., data of each prot
   animal <- as.factor(rep(1:8, each = 2))
   line <- as.factor(rep(c(1,2), each = 8))
   # check if all obsetvations for one Cy are missing or not
+  
   if ((sum(is.na(x[cy==3]))==8|sum(is.na(x[cy==5]))==8) & 
         (sum(is.na(x[line==1]))==8|sum(is.na(x[line==2]))==8)){
     model <- lmer(x~ (1|animal), na.action="na.omit")
-    mean.estimate <- summary(model)$coeff[,1]
-    sd.estimate <- as.vector(sqrt(summary(model)$vcov))
+    mean_est <- summary(model)$coeff[,1]
+    sd_est <- as.vector(sqrt(summary(model)$vcov))
     #str(summary(model))
   } 
   if ((sum(is.na(x[cy==3]))==8|sum(is.na(x[cy==5]))==8) & 
         ((sum(is.na(x[line==1]))!=8)&(sum(is.na(x[line==2]))!=8))){
     model <- lmer(x~ line + (1|animal), na.action="na.omit")
-    mean.estimate <- summary(model)$coeff[1,1] + summary(model)$coeff[2,1]/2
-    sd.estimate <- as.vector(sqrt(t(c(1,1/2)) %*%summary(model)$vcov%*%c(1,1/2)))
+    mean_est <- summary(model)$coeff[1,1] + summary(model)$coeff[2,1]/2
+    sd_est <- as.vector(sqrt(t(c(1,1/2)) %*%summary(model)$vcov%*%c(1,1/2)))
   }
   
   if ((sum(is.na(x[cy==3]))!=8|sum(is.na(x[cy==5]))!=8) & 
         ((sum(is.na(x[line==1]))==8)&(sum(is.na(x[line==2]))==8))){
     model <- lmer(x~ cy + (1|animal), na.action="na.omit")
-    mean.estimate <- summary(model)$coeff[1,1] + summary(model)$coeff[2,1]/2
-    sd.estimate <- as.vector(sqrt(t(c(1,1/2)) %*%summary(model)$vcov%*%c(1,1/2)))
+    mean_est <- summary(model)$coeff[1,1] + summary(model)$coeff[2,1]/2
+    sd_est <- as.vector(sqrt(t(c(1,1/2)) %*%summary(model)$vcov%*%c(1,1/2)))
   }
   
   if ((sum(is.na(x[cy==3]))!=8& sum(is.na(x[cy==5]))!=8) & 
         ((sum(is.na(x[line==1]))!=8)&(sum(is.na(x[line==2]))!=8))){ 
     model <- lmer(x~ cy + line + (1|animal), na.action="na.omit")
     #str(summary(model))
-    mean.estimate <- summary(model)$coeff[1,1] + 
+    mean_est <- summary(model)$coeff[1,1] + 
       summary(model)$coeff[2,1]/2 + 
       summary(model)$coeff[3,1]/2
     
-    sd.estimate <- as.vector(sqrt(t(c(1, 1/2, 1/2))%*% summary(model)$vcov %*%c(1,1/2, 1/2)))  }
+    sd_est <- as.vector(sqrt(t(c(1, 1/2, 1/2))%*% summary(model)$vcov %*%c(1,1/2, 1/2)))  }
   
   
-  return(c(mean.estimate, sd.estimate))
+  return(c(mean_est, sd_est))
 }
-depleted.dat[25,]
 
-dim(depleted.dat)
-dim(not.depleted.dat)
-depleted.out <- laply(1:236, function(i)result(depleted.dat[i,], depleted = "TRUE"))
+depleted_out <- laply(1:236, function(i)out_model(deplete[i,], depleted = "TRUE"))
 
-not.depleted.out <- laply(1:236, function(i)result(not.depleted.dat[i,], depleted = "FALSE"))
+whole_out <- laply(1:236, function(i)out_model(whole[i,], depleted = "FALSE"))
 
-colnames(depleted.out) <- colnames(not.depleted.out) <- c("mean","sd")
+colnames(depleted_out) <- colnames(whole_out) <- c("mean","sd")
+sd_out <- data.frame(
+  logsd = log(c(whole_out[, "sd"], depleted_out[,"sd"])), 
+  sample = rep(c("whole", "depleted"), each = dim(whole_out)[1]))
 
-summary(depleted.out) # depleted group
-summary(not.depleted.out) # not.depleted group 
-# mean_group <- as.data.frame(cbind(value =c(depleted.out[,"mean"], not.depleted.out[,"mean"]), group = rep(c("depleted", "whole"), each = dim(depleted.out)[1])))
-# str(mean_group)
-# qplot(mean_group)
-# qplot(group, value, data = mean_group, geom= "boxplot")
-#boxplot(depleted.out)
-
-
-mean(depleted.out[,2] > not.depleted.out[,2])
+p <- ggplot(sd_out, aes(sample, logsd))
+p + geom_boxplot(aes(fill = sample)) + 
+  ggtitle("Standard Error for Each Sample Type") + 
+  scale_fill_discrete(name= "Sample Type") + 
+  xlab("Sample Type") + 
+  ylab("log(Standard Error)") + 
+  theme(text = element_text(size=18))
 
 
-wilcox.test(depleted.out[,2], not.depleted.out[, 2], alternative = "greater", paired = TRUE)
-wilcox.test(depleted.out[,2], not.depleted.out[, 2], alternative = "two.sided", paired = TRUE)
-?wilcox.test
-wilcox.test(log(depleted.out[,2]/not.depleted.out[, 2]), alternative = "two.sided")
-wilcox.test(log(depleted.out[,2]/not.depleted.out[, 2]), alternative = "greater")
-plot(log(depleted.out[,2]), log(not.depleted.out[, 2]))
-hist(log(depleted.out[,2])-log(not.depleted.out[, 2]), nclass = 30)
-mean(log(depleted.out[,2])-log(not.depleted.out[, 2])>0)
-
-# check for outlier
-sum(abs(depleted.out[,1])>10)
-
-# Delete outlier
-depleted.out2 <- depleted.out[-which(abs(depleted.out[,1])>10),]
-
-not.depleted.out2 <- not.depleted.out[-which(abs(depleted.out[,1])>10),]
-
-summary(depleted.out2) # depleted group
-summary(not.depleted.out2) # not.depleted group 
+wilcox.test(log(depleted_out[,2]), log(whole_out[, 2]), alternative = "greater", paired = TRUE)
+wilcox.test(depleted_out[,2], whole_out[, 2], alternative = "two.sided", paired = TRUE)
 
 
-mean(depleted.out2[,2] > not.depleted.out2[,2])
+wilcox.test(log(depleted_out[,2]/whole_out[, 2]), alternative = "two.sided")
+wilcox.test(log(depleted_out[,2]/whole_out[, 2]), alternative = "greater")
 
-
-wilcox.test(depleted.out2[,2], not.depleted.out2[, 2], alternative = "greater", paired = TRUE)
-wilcox.test(depleted.out2[,2], not.depleted.out2[, 2], alternative = "two.sided", paired = TRUE)
-wilcox.test(log(depleted.out2[,2]/not.depleted.out2[, 2]), alternative = "two.sided")
-wilcox.test(log(depleted.out2[,2]/not.depleted.out2[, 2]), alternative = "greater")
-
-depleted.out2 <- as.data.frame(depleted.out2)
-not.depleted.out2 <- as.data.frame(not.depleted.out2)
-depleted.out2$group <- "depleted"
-not.depleted.out2$group <- "whole"
-out2 <- rbind(depleted.out2, not.depleted.out2)
-
-str(out2)
-boxplot(out2$sd ~ out2$group)
-boxplot(out$sd ~ out$group)
-qplot(y = depleted.out2[,1], x = not.depleted.out2[,1], xlim = c(-8, 8), 
-      ylim = c(-8, 8), main = "mean of each group(deleted outlier)",
-      ylab = "depleted", 
-      xlab = "whole")
-qplot(y = depleted.out2[,2], x = not.depleted.out2[,2], xlim = c(-8, 8), 
-      ylim = c(-8, 8), main = "sd of each group(deleted outlier)",
-      ylab = "depleted", 
-      xlab = "whole")
-
-
-qplot(x = group, y = mean, 
-        data = out2, geom = "boxplot", colour =group, 
-        main = "mean for each group",
-      xlab = )
-
-qplot(x = group, y = sd, 
-      data = out2, geom = "boxplot", colour =group,
-      main = "sd for each group")
-
-  
-
-depleted.out <- as.data.frame(depleted.out)
-not.depleted.out <- as.data.frame(not.depleted.out)
-depleted.out$group <- "depleted"
-not.depleted.out$group <- "whole"
-out <- rbind(depleted.out, not.depleted.out)
-
-
-qplot(y = depleted.out[,1], x = not.depleted.out[,1], xlim = c(-8, 8), 
-      ylim = c(-8, 8), main = "mean of each group(deleted outlier)",
-      ylab = "depleted", 
-      xlab = "whole")
-qplot(y = depleted.out[,2], x = not.depleted.out[,2], xlim = c(-8, 8), 
-      ylim = c(-8, 8), main = "sd of each group(deleted outlier)",
-      ylab = "depleted", 
-      xlab = "whole")
-
-
-#colnames(out)[3] <- "Sample Type"  # log(Standard Error)
-qplot(x = group, y = mean, 
-      data = out, geom = "boxplot", colour =group, 
-      main = "mean for each group")
-
-qplot(x = group, y = log(sd), 
-      data = out, geom = "boxplot", colour =group,
-      main = "Standard Error for Each Sample Type",
-      ylab = "log(Standard Error)", 
-      xlab = "Sample Type")
 
